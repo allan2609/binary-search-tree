2091ebb594f9914a9d543640d862c10e1bd3fcb4
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-07-04 15:12:20 +0300

    Create Node and Tree classes

---
+++ index.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedArray, 0, sortedArray.length - 1);
+  }
+
+  buildTree(array, start, end) {
+    if (start > end) {
+      return null;
+    }
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(array[mid]);
+    node.left = this.buildTree(array, start, mid - 1);
+    node.right = this.buildTree(array, mid + 1, end);
+    return node;
+  }
+}
